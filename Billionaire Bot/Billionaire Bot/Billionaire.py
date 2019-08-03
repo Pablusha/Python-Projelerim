b4c2437ccab1440b5f17580649f483b0139d99b0
Pablusha <53290728+Pablusha@users.noreply.github.com>
2019-08-03 23:20:27 +0300

    Add files via upload

---
+++ Billionaire Bot/Billionaire Bot/Billionaire.py
@@ -0,0 +1,28 @@
+from PIL import ImageGrab,ImageOps
+import pyautogui
+import time
+from numpy import *
+
+def printMousePos(): #974 564
+    while True:
+        time.sleep(1)
+        print(pyautogui.position())
+
+def pressMouseDown():
+    pyautogui.mouseDown()
+
+def imageGrab():
+    box = (974,564,1067,739)
+    image = ImageGrab.grab(box)
+    image = ImageOps.grayscale(image)
+    arr = array(image.getcolors())
+    return arr.sum()
+
+def main():
+   while True:
+       
+       if(imageGrab() == 33216):
+           pressMouseDown()        
+
+
+main()
